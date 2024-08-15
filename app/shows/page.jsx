6b356b4678a3559ff3206d5dd7ccd7b309618713
Muhammad Zain Faraz <bscs21083@itu.edu.pk>
2024-08-15 16:05:32 +0500

    Adding icon to shows

--- app/shows/page.jsx
+++ app/shows/page.jsx
@@ -1,17 +1,15 @@
 import { SearchPage } from './components/shows';
-import Head from 'next/head';
 
 export const metadata = {
   title: 'TV shows',
+  icons: {
+    icon: '/icon.png',
+  },
 };
 
 export default async function Page() {
   return (
     <div>
-      <Head>
-        <title>TV shows</title>
-        <link rel="icon" href="/show-favicon.ico" />
-      </Head>
       <SearchPage />
     </div>
   );
